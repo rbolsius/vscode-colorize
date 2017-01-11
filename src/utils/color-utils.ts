806550b4b0a726e7eef430d042a8be2d90a5c8b1
benjaminJ <kamikillerto@gmail.com>
2017-01-11 13:53:40 +0100

    add testing

---
+++ src/utils/color-utils.ts
@@ -0,0 +1,112 @@
+'use strict';
+
+// const COLOR_REGEX = /(#(?:[\da-f]{3}){1,2}|rgb\((?:\d{1,3},\s*){2}\d{1,3}\)|rgba\((?:\d{1,3},\s*){3}\d*\.?\d+\)|hsl\(\d{1,3}(?:,\s*\d{1,3}%){2}\)|hsla\(\d{1,3}(?:,\s*\d{1,3}%){2},\s*\d*\.?\d+\))/gi
+// const HEXA_COLOR = /#(?:[\da-f]{3}($| |,|;)){1}|(?:(#(?:[\da-f]{3}){2})(\t|$| |,|;))/gi
+
+
+/**
+ * Utils object for color manipulation
+ */
+const COLOR_REGEX= {
+    HEXA_COLOR_SMALL: /(#[\da-f]{3})($|,| |;|\n)/gi,
+    HEXA_COLOR: /(#[\da-f]{6})($| |,|;|\n)/gi,
+};
+
+const ColorUtils = {
+
+  getRGB(color: string): number[] {
+    // var a;
+    // if (b && b.constructor == Array && b.length == 3) return b;
+    // if (a = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(b)) return [parseInt(a[1]), parseInt(a[2]), parseInt(a[3])];
+    // if (a = /rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(b)) return [parseFloat(a[1]) * 2.55, parseFloat(a[2]) * 2.55, parseFloat(a[3]) * 2.55];
+    // if (a = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(b)) return [parseInt(a[1], 16), parseInt(a[2], 16), parseInt(a[3],
+    // 	16)];
+
+    let rgb;
+    if (color.match(this.HEXA_COLOR_SMALL)) {
+      rgb = /#(.+)/gi.exec(color);
+      return rgb[1].split('').map(_ => parseInt(_ + _, 16));
+    }
+    if (color.match(this.HEXA_COLOR)) {
+      rgb = /#(.+)/gi.exec(color);
+      rgb = rgb[1].split('').map(_ => parseInt(_, 16));
+      return [16 * rgb[0] + rgb[1], 16 * rgb[2] + rgb[3], 16 * rgb[4] + rgb[5]];
+    }
+    // if (rgb = /#(.+)/gi.exec(color)) {
+    // 	return rgb[1].split('').map(_ =>  parseInt(_ + _, 16));
+    // }
+    return [];
+    // return (typeof (rgb) != "undefined") ? rgb[rgb.trim().toLowerCase()] : [];
+  },
+
+  luminance(color: string): number {
+    let rgb = this.getRGB(color);
+    if (!rgb) {
+      return null;
+    }// return 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+    return ((rgb[0] * 299) + (rgb[1] * 587) + (rgb[2] * 114)) / 1000;
+  },
+  extractColor(text: string): Object {
+    let match = null;
+    if (match = this.HEXA_COLOR_SMALL.exec(text)) {
+      return {
+        model: 'hex_small',
+        value: match[1]
+      };
+    }
+  }
+};
+
+export { COLOR_REGEX, ColorUtils };
+
+// 'use strict';
+
+// /**
+//  * Utils object for color manipulation
+//  */
+// const ColorUtils = {
+
+// // const COLOR_REGEX = /(#(?:[\da-f]{3}){1,2}|rgb\((?:\d{1,3},\s*){2}\d{1,3}\)|rgba\((?:\d{1,3},\s*){3}\d*\.?\d+\)|hsl\(\d{1,3}(?:,\s*\d{1,3}%){2}\)|hsla\(\d{1,3}(?:,\s*\d{1,3}%){2},\s*\d*\.?\d+\))/gi
+// // const HEXA_COLOR = /#(?:[\da-f]{3}($| |,|;)){1}|(?:(#(?:[\da-f]{3}){2})(\t|$| |,|;))/gi
+
+// 	HEXA_COLOR_SMALL: /#[\da-f]{3}(?:$| |,|;)/gi,
+// 	HEXA_COLOR: /#[\da-f]{6}(?:$| |,|;)/gi,
+
+// 	getRGB(color:string):number[] {
+// 		// var a;
+// 		// if (b && b.constructor == Array && b.length == 3) return b;
+// 		// if (a = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(b)) return [parseInt(a[1]), parseInt(a[2]), parseInt(a[3])];
+// 		// if (a = /rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(b)) return [parseFloat(a[1]) * 2.55, parseFloat(a[2]) * 2.55, parseFloat(a[3]) * 2.55];
+// 		// if (a = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(b)) return [parseInt(a[1], 16), parseInt(a[2], 16), parseInt(a[3],
+// 		// 	16)];
+
+// 		let rgb;
+// 		if (color.match(ColorUtils.HEXA_COLOR_SMALL)) {
+// 			rgb = /#(.+)/gi.exec(color)
+// 			return rgb[0].split('').map(_ =>  parseInt(_ + _, 16));
+// 		}
+// 		if (color.match(ColorUtils.HEXA_COLOR)) {
+// 			rgb = /#(.+)/gi.exec(color)
+// 			rgb = rgb[0].split('').map(_ =>  parseInt(_, 16));
+// 			return [rgb[0]+rgb[1],rgb[2]+rgb[3],rgb[4]+rgb[5]];
+// 		}
+// 		// if (rgb = /#(.+)/gi.exec(color)) {
+// 		// 	return rgb[1].split('').map(_ =>  parseInt(_ + _, 16));
+// 		// }
+// 		return [];
+// 		// return (typeof (rgb) != "undefined") ? rgb[rgb.trim().toLowerCase()] : [];
+// 	},
+
+//    luminance(color:string):number {
+// 		var rgb = ColorUtils.getRGB(color);
+// 		if (!rgb) return null;
+// 		// return 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+// 		return ((rgb[0] * 299) + (rgb[1] * 587) + (rgb[2] * 114)) / 1000;
+// 	},
+// 	extractColor(text:string):Object {
+// 		let match = null;
+// 		if (match = ColorUtils.HEXA_COLOR_SMALL.exec(text))
+// 		return {};
+// 	}
+// };
+// export default ColorUtils;
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -4,19 +4,41 @@
 //
 
 // The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
+import { assert } from 'chai';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
+import { COLOR_REGEX } from '../src/utils/color-utils';
+
+const { HEXA_COLOR_SMALL } = COLOR_REGEX
+
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+describe("COLOR_REGEX Tests", () => {
 
     // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
+    // describe("css hexa shorthand color", () => {
+      it('Should match color with only integer', function() {
+        return assert.equal(HEXA_COLOR_SMALL.test('#000'), true);
+      });
+
+      it('Should match color with letters and integers', function() {
+        return assert.equal(HEXA_COLOR_SMALL.test('#f0a'), true);
+      });
+
+      it('Should match color with only letters', function() {
+        return assert.equal(HEXA_COLOR_SMALL.test('#fff'), true);
+      });
+
+      it('Regex should not care about the case', function() {
+        return assert.equal(HEXA_COLOR_SMALL.test('#Abc'), true);
+      });
+
+      it('Should not match', function() {
+        return assert.equal(HEXA_COLOR_SMALL.test('#AbG'), false);
+      });
+    // });
 });
+
